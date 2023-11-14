80f2d7c1de417a5668e806a1d7cdd0feec5fab34
Soumadipta Roy <roysouma@amazon.com>
2023-11-14 14:53:02 +0000

    Add business logic and correct deployment zip file.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-exports.handler = async (evt) => {
-   // hotel's booking process
+const fetch = require("node-fetch");
 
-   return var;
+exports.handler = async (evt) => {
+// make request
+let response = await fetch("https://njfn2y5r0h.execute-api.us-west-2.amazonaws.com/Prod/hotel", {
+                  method: 'POST',
+                  body: JSON.stringify(evt)
+               });
+
+// get the response as json
+let json = await response.json();
+
+return json;
 }
 
